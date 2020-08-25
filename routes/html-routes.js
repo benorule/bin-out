f95d43d680c5e80915b4ce78053833cb8ca67861
marasinipradeep <marasinipradeep@gmail.com>
2020-08-25 18:02:17 +0930

    #Admin username and password authetication check completed

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,8 @@ const path = require("path");
 const db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated")
+const isAdminAuthenticated = require("../config/middleware/isAdminAuthenticated");
 
 module.exports = function (app) {
 
@@ -14,7 +15,7 @@ module.exports = function (app) {
 
   //Routes for signing up user information
   app.get("/user/signup", (req, res) => {
-    // If the user already has an account send them to the members page
+    // If the user already has an account send them to the login page
     if (req.user) {
       res.redirect("/user/dashboard");
     }
@@ -51,13 +52,19 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/userdashboard.html"));
   });
 
+  //Routes for admin login
   //Routes for admin dashboard which will populate users table initially
-  app.get("/admin/dashboard", (req, res) => {
+  app.get("/admin", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/adminloginpage.html"));
+  });
+
+  //Routes for admin dashboard which will populate users table initially
+  app.get("/admin/dashboard",isAdminAuthenticated, (req, res) => {
     res.redirect("/admin/users");
   });
 
   //Routes to find all the users from database which will populate on the users table
-  app.get("/admin/users", (req, res) => {
+  app.get("/admin/users",isAdminAuthenticated, (req, res) => {
     db.User.findAll({
       where:{
         approvedUser:false
@@ -74,7 +81,7 @@ module.exports = function (app) {
 
 
   //Routes for getting all the employees from the database and populates on employee table
-  app.get("/admin/employees", (req, res) => {
+  app.get("/admin/employees",isAdminAuthenticated, (req, res) => {
     db.Employee.findAll({
       where:{
         approvedEmployee:false
